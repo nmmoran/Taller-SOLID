5e018ad05df3ea8d856289e13d3bab8f54b3603c
BurneoDanny <djburneo@espol.edu.ec>
2021-10-27 20:19:39 -0500

    Cambio de clases Helado y Pastel y nueva clase Postre

--- TallerSOLID/src/Postres/Helado.java
+++ TallerSOLID/src/Postres/Helado.java
@@ -1,41 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Postres;
 
-import java.util.ArrayList;
 import Adicionales.Aderezo;
+import java.util.ArrayList;
 
-/**
- *
- * @author Pedro Mendoza
- */
-public class Helado{
-    private String sabor;
-    private double precioParcial;
-    private ArrayList<Aderezo> aderezos;
+public class Helado extends Postre{
+
     
-    public Helado(String sabor){
-        aderezos= new ArrayList<>();
-        this.sabor=sabor;
-        this.precioParcial = 7.85;
+    public Helado(String sabor, double precioParcial, ArrayList<Aderezo> aderezos){
+        super(sabor, precioParcial, aderezos);
+        precioParcial = 7.85;
     }
     
     public double calcularPrecioFinal(){
         double precioFinal;
-        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
+        precioFinal=(super.getPrecioParcial()+(super.getPrecioParcial()*0.12))+(super.getAderezos().size()*0.50);
         return precioFinal;
     }
 
-    public ArrayList<Aderezo> getAderezos() {
-        return aderezos;
-    }
 
     @Override
     public String toString() {
-        return "Helado{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
+        return "Helado{" + "sabor=" + super.getSabor() + ", precioParcial=" + super.getPrecioParcial() + ", aderezos=" + super.getAderezos() + '}';
     }
     
     public String showPrecioFinal(){
---
+++ TallerSOLID/src/Postres/Postre.java
@@ -0,0 +1,43 @@
+package Postres;
+
+import java.util.ArrayList;
+
+import Adicionales.Aderezo;
+
+public class Postre {
+    private String sabor;
+    private double precioParcial;
+    private ArrayList<Aderezo> aderezos;
+    
+    public Postre(String sabor, double precioParcial, ArrayList<Aderezo> aderezos) {
+    this.sabor = sabor;
+    this.precioParcial = precioParcial;
+    this.aderezos = aderezos;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public double getPrecioParcial() {
+        return precioParcial;
+    }
+
+    public void setPrecioParcial(double precioParcial) {
+        this.precioParcial = precioParcial;
+    }
+
+    public ArrayList<Aderezo> getAderezos() {
+        return aderezos;
+    }
+
+    public void setAderezos(ArrayList<Aderezo> aderezos) {
+        this.aderezos = aderezos;
+    }
+    
+    
+}
